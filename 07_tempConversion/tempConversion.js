bcce79dbf1bcce6ee54ec228528b2b4ea81b9e9f
Igor <mr.igor187@gmail.com>
2022-06-11 18:00:32 +0200

    finish fundamentals 4

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,10 @@
-const ftoc = function() {
-
+const ftoc = function(fahr, cels) {
+  let result = (fahr-32) * 5/9;
+  return Math.round(result * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(cels, fahr) {
+  return Math.round(((cels * 9/5) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
