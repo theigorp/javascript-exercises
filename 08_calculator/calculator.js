70dce2b06f20e3f03077d886bb3db5fda0248586
Igor <mr.igor187@gmail.com>
2022-07-05 12:45:16 +0200

    pass all test 08calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+  if(nums.length < 1) return 0;
+	else return nums.reduce((sum, num) => sum+num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  return nums.reduce((product, num) => product*num, 1);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let factorial = 1;
+
+  if(num == 0 || num == 1) return factorial;
+  
+	for(let i = num; i > 0; i--)
+  {
+    factorial*=i;
+  }
+
+  return factorial;
 };
 
 // Do not edit below this line
