2b2a1f21a32c65aa2d97d745bb58e5670e34dd2a
Igor <mr.igor187@gmail.com>
2022-07-05 13:25:39 +0200

    pass all test 09palindromes, finally understood palindrome for loop checking - see solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let newString = string.toLowerCase().replaceAll(' ', '').replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
 
+    // if(newString == newString.split('').reverse().join('')) return true;
+    // else return false;
+    
+    for(let i = 0; i < newString.length/2; i++)
+    {
+        if(newString[i] != newString[newString.length-1-i]) return false;
+                
+    }
+    return true;
 };
 
 // Do not edit below this line
