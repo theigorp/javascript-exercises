027e85acc0787539b0d42972cc973ff50ca3920b
Igor <mr.igor187@gmail.com>
2022-07-05 14:05:17 +0200

    pass all tests 10fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    if(number < 0) return 'OOPS';
 
+    if(number == 1) return 1;
+
+    let n1=0, n2=1, current;
+
+    for(let i = 0; i < number-1; i++)
+    {
+        current = n1+n2;
+        n1 = n2;
+        n2 = current;
+    }
+
+    return current;
 };
 
 // Do not edit below this line
